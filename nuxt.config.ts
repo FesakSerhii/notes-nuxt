5113c59dc756e952338e67ee6a3100278e3bf8f2
serhiyfesak <serhiyfesak@gmail.com>
2023-10-16 19:49:43 +0300

    feat: add styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "UTF-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Notes app"
+    }
+  },
+  css: ["~/assets/scss/main.scss"],
   devtools: { enabled: true }
 })
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+  @import "@/assets/scss/pages/home.scss";
+
+</style>
